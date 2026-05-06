d69afd9bcc88ebe8691cd306c3b1381fe296f8f1
Joseph Wang <jwang8@cnm.edu>
2026-05-06 15:55:43 -0600

    test

---
+++ JenkinsResearch.py
@@ -0,0 +1,30 @@
+import os
+List1 = [1,2,3,4,5,6,7,8]
+List1.reverse()
+
+print("Reversed list is", List1)
+
+
+List2 = [1,5,3,6,2,78,23,45,23,54,22]
+
+List2.sort()
+
+print("Sorted list is", List2)
+
+
+List3 = []
+
+List3 = List3.copy()
+
+print("List3 = ", List3)
+
+indexvalue = List2[2:6]
+
+print("The index values are", indexvalue)
+
+
+
+
+
+
+
